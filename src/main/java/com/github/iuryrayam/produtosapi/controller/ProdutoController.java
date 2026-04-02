dc96aece44e8261f1fb917138403b5230e7538fa
IuryRayam <iury2155@gmail.com>
2026-04-02 15:14:19 -0300

    creating ProdutoRepository

--- src/main/java/com/github/iuryrayam/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/github/iuryrayam/produtosapi/controller/ProdutoController.java
@@ -1,18 +1,32 @@
 package com.github.iuryrayam.produtosapi.controller;
 
 import com.github.iuryrayam.produtosapi.model.Produto;
+import com.github.iuryrayam.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("produtos")
 public class ProdutoController {
 
+    private final ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
     public Produto salvar(@RequestBody Produto produto) {
         System.out.println("produto recebido: " + produto);
+
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+
+        produtoRepository.save(produto);
         return produto;
     }
 }
