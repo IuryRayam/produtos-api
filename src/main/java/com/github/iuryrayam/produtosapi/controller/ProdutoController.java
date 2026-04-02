e0918957ca589a5a49b4811b469eb1865a5f8d08
IuryRayam <iury2155@gmail.com>
2026-04-02 15:42:29 -0300

    add method to get product by id

--- src/main/java/com/github/iuryrayam/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/github/iuryrayam/produtosapi/controller/ProdutoController.java
@@ -2,10 +2,7 @@ package com.github.iuryrayam.produtosapi.controller;
 
 import com.github.iuryrayam.produtosapi.model.Produto;
 import com.github.iuryrayam.produtosapi.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -28,5 +25,10 @@ public class ProdutoController {
 
         produtoRepository.save(produto);
         return produto;
+    }
+
+    @GetMapping("{id}")
+    public Produto ObterPorId(@PathVariable("id") String id) {
+        return produtoRepository.findById(id).orElse(null);
     }
 }
