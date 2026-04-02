0c6bfc5200e217074359a812f39a85ed10df7024
IuryRayam <iury2155@gmail.com>
2026-04-02 11:57:39 -0300

    started project

---
+++ src/test/java/com/github/iuryrayam/produtosapi/ProdutosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.iuryrayam.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
